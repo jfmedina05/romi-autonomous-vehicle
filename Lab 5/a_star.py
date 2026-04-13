fa007a064ea5703e6cd3587c306e9aebc9d82d33
jfmedina <jfmedina@iu.edu>
2026-04-13 18:02:49 -0400

    New Lab 5

--- Lab 5/a_star.py
+++ Lab 5/a_star.py
@@ -1,57 +1,60 @@
-# Copyright Pololu Corporation.  For more information, see https://www.pololu.com/
 import smbus
 import struct
 import time
 
+
 class AStar:
-  def __init__(self):
-    self.bus = smbus.SMBus(1)
+    def __init__(self, address=20, bus_num=1):
+        self.address = address
+        self.bus = smbus.SMBus(bus_num)
 
-  def read_unpack(self, address, size, format):
-    # Ideally we could do this:
-    #    byte_list = self.bus.read_i2c_block_data(20, address, size)
-    # But the AVR's TWI module can't handle a quick write->read transition,
-    # since the STOP interrupt will occasionally happen after the START
-    # condition, and the TWI module is disabled until the interrupt can
-    # be processed.
-    #
-    # A delay of 0.0001 (100 us) after each write is enough to account
-    # for the worst-case situation in our example code.
+    def read_unpack(self, register, size, fmt):
+        for i in range(5):
+            try:
+                self.bus.write_byte(self.address, register)
+                time.sleep(0.0001)
+                byte_list = [self.bus.read_byte(self.address) for _ in range(size)]
+                return struct.unpack(fmt, bytes(byte_list))
+            except OSError:
+                time.sleep(0.01)
+                if i == 4:
+                    raise
 
-    self.bus.write_byte(20, address)
-    time.sleep(0.0001)
-    byte_list = [self.bus.read_byte(20) for _ in range(size)]
-    return struct.unpack(format, bytes(byte_list))
+    def write_pack(self, register, fmt, *data):
+        data_array = list(struct.pack(fmt, *data))
+        for i in range(5):
+            try:
+                self.bus.write_i2c_block_data(self.address, register, data_array)
+                time.sleep(0.0001)
+                return
+            except OSError:
+                time.sleep(0.01)
+                if i == 4:
+                    raise
 
-  def write_pack(self, address, format, *data):
-    data_array = list(struct.pack(format, *data))
-    self.bus.write_i2c_block_data(20, address, data_array)
-    time.sleep(0.0001)
+    def motors(self, left, right):
+        self.write_pack(6, "hh", left, right)
 
-  def leds(self, red, yellow, green):
-    self.write_pack(0, 'BBB', red, yellow, green)
+    def read_battery_millivolts(self):
+        return self.read_unpack(10, 2, "H")
 
-  def play_notes(self, notes):
-    self.write_pack(24, 'B14s', 1, notes.encode("ascii"))
+    def read_analog(self):
+        return self.read_unpack(12, 12, "HHHHHH")
 
-  def motors(self, left, right):
-    self.write_pack(6, 'hh', left, right)
+    def read_encoders(self):
+        return self.read_unpack(39, 4, "hh")
 
-  def read_buttons(self):
-    return self.read_unpack(3, 3, "???")
+    def set_auto_mode(self, enable):
+        self.write_pack(43, "?", enable)
 
-  def read_battery_millivolts(self):
-    return self.read_unpack(10, 2, "H")
+    def trigger_calibration(self):
+        self.write_pack(44, "?", True)
 
-  def read_analog(self):
-    return self.read_unpack(12, 12, "HHHHHH")
+    def check_if_calibrating(self):
+        return self.read_unpack(44, 1, "?")[0]
 
-  def read_encoders(self):
-    return self.read_unpack(39, 4, 'hh')
+    def write_pid(self, kp, ki, kd):
+        self.write_pack(45, "fff", kp, ki, kd)
 
-  def test_read8(self):
-    self.read_unpack(0, 8, 'cccccccc')
-
-  def test_write8(self):
-    self.bus.write_i2c_block_data(20, 0, [0,0,0,0,0,0,0,0])
-    time.sleep(0.0001)
+    def read_p5_telemetry(self):
+        return self.read_unpack(57, 8, "fhh")
